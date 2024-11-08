61b6ba2f744c6e232b438e67bcde6514a687e2c0
Yuri Moreira <128077291+yurimoreira08@users.noreply.github.com>
2024-11-08 07:42:17 -0300

    Exercício 1

---
+++ MeuPrograma.java
@@ -0,0 +1,44 @@
+/* 1
+public class MeuPrograma {
+    public static void main(String[] args) {
+        for (int i = 150; i < 301; i++) {
+        if (i < 151 && i > 301) {
+        continue;
+        }
+        System.out.println(i);
+        }
+    }
+}
+*/
+
+/* 2
+public class MeuPrograma {
+    public static void main(String[] args) {
+        int soma = 0;
+        for (int i = 1; i <= 1000; i++) {
+            soma += i;
+        }
+        System.out.println(i);
+    }
+}
+*/
+
+/* 3
+public class MeuPrograma {
+    public static void main(String[] args) {
+    }
+}
+*/
+
+/* 4 */
+public class MeuPrograma {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            long fatorial = 1;
+        }
+        for (int j = 1; j <= 1; j++){
+            fatorial = j;
+        }
+        System.out.println(soma);
+    }
+}
